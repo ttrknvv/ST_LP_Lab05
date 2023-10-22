e10ecec2f0106329dc04425a6ebbb2702c8e72a4
ttrknvv <chernaya.panterka.69@gmail.com>
2023-10-22 23:21:40 +0300

    end labs

---
+++ maven-demo/src/main/java/com/ttrknvv/automation/App.java
@@ -0,0 +1,13 @@
+package com.ttrknvv.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
